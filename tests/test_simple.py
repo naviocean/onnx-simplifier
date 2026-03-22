d52690ca517752354d63c8dbf8789285a25006a6
take-cheeze <takechi101010@gmail.com>
2026-03-22 14:33:28 +0900

    Add test for #8
    closes #8

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -60,3 +60,48 @@ def test_transformer():
         torch.rand((15, 2, 256), dtype=torch.float32),
     )
     export_simplify_and_check_by_python_api(model, inputs, export_kwargs={"dynamo": True})
+
+
+def test_upsample():
+    import torch.nn.functional as F
+
+    class Net(torch.nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1_1 = torch.nn.Conv2d(3, 16, 3, padding=1)
+            self.conv1_2 = torch.nn.Conv2d(16, 16, 3, padding=1)
+            self.bn1 = torch.nn.BatchNorm2d(16)
+            self.maxpool = torch.nn.MaxPool2d(2, stride=2)
+            self.conv2_1 = torch.nn.Conv2d(16, 32, 3, padding=1)
+            self.conv2_2 = torch.nn.Conv2d(32, 32, 3, padding=1)
+            self.bn2 = torch.nn.BatchNorm2d(32)
+            self.conv3_1 = torch.nn.Conv2d(32, 16, 3, padding=1)
+            self.conv3_2 = torch.nn.Conv2d(16, 16, 3, padding=1)
+            self.conv3_3 = torch.nn.Conv2d(16, 3, 3, padding=1)
+            self.bn3 = torch.nn.BatchNorm2d(3)
+
+        def forward(self, x):
+            x1 = F.relu(self.bn1(self.conv1_2(F.relu(self.conv1_1(x)))))
+            x2 = self.maxpool(x1)
+            xup = F.interpolate(x2, scale_factor=2, mode='bilinear', align_corners=False)
+            x3 = self.bn2(self.conv2_2(F.relu(self.conv2_1(xup))))
+            x4 = F.relu(self.conv3_3(self.conv3_2(F.relu(self.conv3_1(x3)))))
+            x5 = self.bn3(x4)
+            return F.softsign(x5)
+
+
+    inp = torch.rand(1, 3, 96, 96)
+    net = Net()
+    opt = export_simplify_and_check_by_python_api(net, (inp,), export_kwargs={"opset_version": 9})
+
+    u_out = None
+    for n in opt.graph.node:
+        if n.op_type == "Upsample":
+            u_out = n.output[0]
+    assert u_out is not None
+    u_info = None
+    for v in opt.graph.value_info:
+        if v.name == u_out:
+            u_info = v
+    assert u_info is not None
+    assert [i.dim_value for i in v.type.tensor_type.shape.dim] == [1, 3, 96, 96]
